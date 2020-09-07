bb3d2fab643059c898b8ad3490ef8d5a506d93a4
Rajesh jaga <Rajaga223@gmail.com>
2020-09-07 10:39:13 +0530

    accordian applied according to ux

--- script/index.js
+++ script/index.js
@@ -1,9 +1,35 @@
 // import { } from './import';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    //Variables declaration
     const navButton = document.getElementById('nav-button');
+    const ul = document.querySelector('ul');
+    const navBar = document.querySelectorAll('#nav-bar');
+    const accordiano = document.getElementById('accordian');
+    const closeAccordiano = document.getElementById('closeaccordian');
+    const disabledCards = document.querySelectorAll('.disabled');
+    //event listeners
     navButton.addEventListener('click', () => {
-        document.querySelector('ul').classList.toggle('show');
+        ul.classList.toggle('show');
+        navBar.forEach(bar => { bar.classList.toggle('show'); });
+    });
+    //accordian
+    accordiano.addEventListener('click', () => {
+        disabledCards.forEach(card => {
+            if (card.classList.contains('disabled')) {
+                card.style.display = 'block';
+                accordiano.style.display = 'none';
+                closeAccordiano.style.display = 'block';
+            }
+        })
+    });
+    closeAccordiano.addEventListener('click', () => {
+        disabledCards.forEach(card => {
+            if (card.classList.contains('disabled')) {
+                card.style.display = 'none';
+                accordiano.style.display = 'block';
+                closeAccordiano.style.display = 'none';
+            }
+        })
     })
-})
+})  
